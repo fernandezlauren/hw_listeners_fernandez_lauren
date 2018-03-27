833723a83b79b28bc9b8f358b5aeba7b50e91b3f
Lauren Fernandez <lfernan60@ufl.edu>
2018-03-26 20:56:22 -0400

    Add comments to JS file

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-//event 1 is mousemove and triggers when the mouse is moved over the section h1 element.
 var x = 0;
 var y = 0;
 var z = 0;
 $(document).ready(function(){
+    //mouseover triggers on footer when the mouse pointer enters the div element, and its child elements.
     $("div.over").mouseover(function(){
         $(".over span").text(x += 1);
     });
+//mouseenter triggers on figcaption only when the mouse pointer enters the div element.
     $("div.enter").mouseenter(function(){
         $(".enter span").text(y += 1);
     });
+//mousemove triggers on section header every time the mouse pointer is moved over the div element.
     $("div.move").mousemove(function(){
         $(".move span").text(z += 1);
     });
