6d37804939ae11607c1b4c649d16299645ea02af
Lauren Fernandez <lfernan60@ufl.edu>
2018-03-26 20:23:56 -0400

    add javascript for onsubmit event listener

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-<form onsubmit="myFunction()">
+<form onsubmit= "formlistener()" >
     Enter name: <input type="text">
     <input type="submit">
+
+    console.log(document.getElementById('firstname').value);
+    console.log(document.getElementById('lastname').value);
+    console.log(document.getElementById('email').value);
+    console.log(document.getElementById('message').value);
+
+$("form").submit(function(e) {
+    e.preventDefault();
+});
+
 </form>
