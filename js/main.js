ca4ba7ea965229bee2fc86921bfe25eddeb9b8dd
Lauren Fernandez <lfernan60@ufl.edu>
2018-03-26 20:51:04 -0400

    three different event listeners added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-//event 1 is mousemove and triggers when the mouse is moved over the section h1 element. 
+//event 1 is mousemove and triggers when the mouse is moved over the section h1 element.
+var x = 0;
+var y = 0;
+var z = 0;
 $(document).ready(function(){
+    $("div.over").mouseover(function(){
+        $(".over span").text(x += 1);
+    });
+    $("div.enter").mouseenter(function(){
+        $(".enter span").text(y += 1);
+    });
     $("div.move").mousemove(function(){
         $(".move span").text(z += 1);
-    };
+    });
 });
 
-//event 2
 
 //event 3
