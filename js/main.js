dab79e25f06c6e1be5f7736cb1fda4212975a1fd
Lauren Fernandez <lfernan60@ufl.edu>
2018-03-26 20:39:29 -0400

    changes to js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//event 1 is mousemove and triggers when the mouse is moved over the section h1 element. 
+$(document).ready(function(){
+    $("div.move").mousemove(function(){
+        $(".move span").text(z += 1);
+    };
+});
+
+//event 2
+
+//event 3
